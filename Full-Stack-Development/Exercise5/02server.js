99621363507ace5f1a2729f47a8ee17ad75aaa75
boggarapuYash <yaswanth7868@outlook.com>
2023-03-21 15:19:55 +0530

    Exercise 5

--- Full-Stack-Development/Exercise5/02server.js
+++ Full-Stack-Development/Exercise5/02server.js
@@ -2,10 +2,11 @@ var net = require("net");
 var server = net.createServer(function(connection) 
 {
     console.log('Client connected.');
-    connection.on('end', function()
+    connection.on('end', function() 
     {
-        console.log('Client disconnected.')
+        console.log('Client disconnected.');
     });
+    connection.write("Yash");
     connection.pipe(connection);
 });
 
