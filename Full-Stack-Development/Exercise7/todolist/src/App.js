d4a42af21fb4c70bf721508e4abbeb1afdd1271b
boggarapuYash <yaswanth7868@outlook.com>
2023-03-28 00:20:18 +0530

    Exercise 8

--- Full-Stack-Development/Exercise7/todolist/src/App.js
+++ Full-Stack-Development/Exercise7/todolist/src/App.js
@@ -1,32 +1,91 @@
-import './App.css';
-import  React, {useState} from "react";
+import React from "react";
+import "./App.css";
+import { Button, Card, Form } from 'react-bootstrap';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+function Todo({ todo, index, markTodo, removeTodo }) {
+  return (
+    <div
+      className="todo"
+      
+    >
+      <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
+      <div>
 
-
-function ToDoList() {
-  const [todos, SetTodos] = useState([]);
-  const [newTodo, setNewTodos] = useState('');
-  const handleSbumit = (event)=>{
-    event.preventDefault();
-    if(newTodo.trim())
-    {
-      SetTodos([...todos, newTodo.trim()]);
-      setNewTodos('');
-    }
-  };
-  const handleDelete=(index)=>{
-    const updatedTodos = todos.filter((todos,i)=>i!==index);
-    SetTodos(updatedTodos);
-  };
-
-  return(
-    <div>
-      <form onSubmit={handleSbumit}>
-        <input type = "text" value={newTodo} onChange={}></input>
-      </form>
+        <Button variant="primary" onClick={() => markTodo(index)}>✓</Button>{' '}
+        <Button variant="outline-danger" onClick={() => removeTodo(index)}>✕</Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ToDoList;
+function FormTodo({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}> 
+    <Form.Group>
+      <Form.Label><b>Add Todo</b></Form.Label>
+      <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
+    </Form.Group>
+    <Button variant="primary mb-3" type="submit">
+      Submit
+    </Button>
+  </Form>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = React.useState([ ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const markTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isDone = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="container">
+        <h1 className="text-center mb-4">Todo List</h1>
+        <FormTodo addTodo={addTodo} />
+        <div>
+          {todos.map((todo, index) => (
+            <Card>
+              <Card.Body>
+                <Todo
+                key={index}
+                index={index}
+                todo={todo}
+                markTodo={markTodo}
+                removeTodo={removeTodo}
+                />
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- Full-Stack-Development/Exercise8/imageslider/src/App.js
+++ Full-Stack-Development/Exercise8/imageslider/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const Images =[    
+    {
+      url: "C://Users//HP//Desktop//Taboo//Sem VI//LAB//Full Stack Development//Full-Stack-Development//Exercise8//imageslider//src//images//1.jpg"
+    },
+    {
+      url: "C://Users//HP//Desktop//Taboo//Sem VI//LAB//Full Stack Development//Full-Stack-Development//Exercise8//imageslider//src//images//2.jpg"
+    },
+
+    {
+      url: "C://Users//HP//Desktop//Taboo//Sem VI//LAB//Full Stack Development//Full-Stack-Development//Exercise8//imageslider//src//images//3.jpg"
+    },
+    {
+      url: "C://Users//HP//Desktop//Taboo//Sem VI//LAB//Full Stack Development//Full-Stack-Development//Exercise8//imageslider//src//images//4.jpg"
+    },
+    {
+      url: "C://Users//HP//Desktop//Taboo//Sem VI//LAB//Full Stack Development//Full-Stack-Development//Exercise8//imageslider//src//images//5.jpg"
+    }                
+  ];
+  const [activeImage, setCurrent] = useState(0);
+  const length = Images.length;
+  const nextSlide = ()=>{
+    setCurrent(activeImage === length -1 ? 0: activeImage+1);
+  };
+  const prevSlide = ()=>{
+    setCurrent(activeImage === 0 ? length-1: activeImage-1);
+  };
+  if(!Array.isArray(Images)||Images.length <= 0)
+  {
+    return null;
+  }
+  return(
+    <div>
+      <h1 style={{textAlign:'center'}}>Image Slider</h1>
+       <section class = "imageslider">
+        <div class = "left">
+          <ArrowBackIosIcon onClick = {prevSlide}/>
+        </div>
+        <div class = "right">
+          <ArrowForwardIosIcon onClick = {nextSlide}/>
+        </div>
+        {
+          Images.map((currentSlide, ind)=>{
+            return(
+              <div className={ind === activeImage ? "currentSlide active":"CurrentSlide"}
+              key = {ind}>
+                 {ind === activeImage && <img src= {currentSlide.url} className="image" alt= "not loaded" />}
+              </div>
+            )
+          })
+        }     
+       </section>
     </div>
-  );
+  )
 }
 
 export default App;
